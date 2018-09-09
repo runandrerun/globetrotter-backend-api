b815fe1e1f8fe6e69177c935feed3f63fe535caa
runandrerun <the.asantiagojr@gmail.com>
2018-09-09 13:28:07 -0400

    fixed relationship errors

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,5 +14,6 @@
 
 class Location < ApplicationRecord
   has_many :events
+  has_many :trip_locations
   has_many :trips, through: :trip_locations
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -18,5 +18,6 @@
 
 class Trip < ApplicationRecord
   belongs_to :user
+  has_many :trip_locations
   has_many :locations, through: :trip_locations
 end
